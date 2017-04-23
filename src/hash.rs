3f0182fcfe56c8638aaba35e5f4ae999f430c679
Michael Sproul <micsproul@gmail.com>
2017-04-23 14:16:38 +1000

    Change hashing scheme to be more like the one used in Certificate Transparency.

--- src/hash.rs
+++ src/hash.rs
@@ -1,3 +1,11 @@
+//! Hashing scheme used is:
+//!
+//! hash(file) = H(0x00 || contents)
+//! hash(dir) = H(0x01 || hash(child0) || hash(child1) || ...)
+//!
+//! This is similar to the Merkle tree used in certificate transparency (but with n-way branching).
+//! See: https://tools.ietf.org/html/rfc6962#section-2.1
+
 use std::path::Path;
 use std::io;
 use std::io::prelude::*;
@@ -6,11 +14,13 @@ use ring::digest::{Context, Digest, SHA256};
 
 const BUF_SIZE: usize = 4096;
 
+/// Hash a file by computing H(0x00 || file_contents).
 pub fn hash_file(path: &Path) -> io::Result<Digest> {
     let mut context = Context::new(&SHA256);
     let mut buffer = vec![0; BUF_SIZE];
     let mut file = File::open(path)?;
 
+    context.update(&[0x00]);
     loop {
         let bytes_read = file.read(&mut buffer)?;
 
@@ -24,11 +34,11 @@ pub fn hash_file(path: &Path) -> io::Result<Digest> {
     Ok(context.finish())
 }
 
-/// Combine digests by hashing them together, like in a Merkle tree.
-/// FIXME: not secure currently due to conflation of internal nodes + leaf nodes.
+/// Hash a directory, given hashes for its children. Hash is H(0x01 || d0 || d1 || ...).
 pub fn combine_digests(digests: Vec<&Digest>) -> Digest {
     let mut context = Context::new(&SHA256);
 
+    context.update(&[0x01]);
     for digest in digests {
         context.update(digest.as_ref());
     }
